c45d1e90d1eb1c6dba3b6165108f0b8d5559b7d7
zahra <zahraabdallajin@gmail.com>
2024-06-14 09:42:34 +0300

    added somthing new

--- frontend/src/components/Nav.jsx
+++ frontend/src/components/Nav.jsx
@@ -7,8 +7,7 @@ function Nav(){
           <div className="container  flex justify-between  px-10 py-5  items-end">
             <ul className="flex flex-row gap-5 items-center">
             <Link to="/"><li>Home</li></Link>
-                <Link to="/about"><li>About us</li></Link>
-               <Link to="/login">  <li>login</li></Link>
+                <Link to="/login">  <li>login</li></Link>
                <Link to="/active"><li>Activites</li></Link>
                <Link to="/don"><li>Donate</li></Link>
             </ul>
